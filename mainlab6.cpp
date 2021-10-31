c511632f72296b4032a10008ead35cc04f8d258e
el3m3nTiC <90087363+el3m3nTik@users.noreply.github.com>
2021-10-31 22:21:18 +0200

    made another changes /w code

--- mainlab6.cpp
+++ mainlab6.cpp
@@ -3,36 +3,34 @@
 #include "simple.h"
 #include "sorting.h"
 
-using namespace std;
 
 int main() {
-	int matrix[3][3] = {
-		{3, 2, 5},
-		{32, 12, 0},
-		{12, 23, 22}
-	};
+    int matrix[3][3] = {
+            {3,  2,  5},
+            {32, 12, 0},
+            {12, 23, 22}
+    };
 
-	bool flag1, flag2 = true;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			if (simple(matrix[i][j])) {
-				flag1 = false;
-				break;
-			}
-			else if (matrix[i][j] == 0) {
-				flag2 = false;
-				break;
-			}
-		}
-	}
-	if (flag1 == 0 || flag2 == 0) {
-		sorting(matrix);
-	}
+    bool flag1, flag2 = true;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (simple(matrix[i][j])) {
+                flag1 = false;
+                break;
+            } else if (matrix[i][j] == 0) {
+                flag2 = false;
+                break;
+            }
+        }
+    }
+    if (flag1 == 0 || flag2 == 0) {
+        sorting(matrix);
+    }
 
-	for (int i = 0; i < SIZE; i++) {
-		for (int j = 0; j < SIZE; j++)
-			std::cout << matrix[i][j] << " ";
-		std::cout << std::endl;
-	}
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++)
+            std::cout << matrix[i][j] << " ";
+        std::cout << std::endl;
+    }
 
 }
